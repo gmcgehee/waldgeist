aaa083c735ffe643a444adcb945bbcf785e365da
gmcgehee <geist.mcgehee@eagles.cui.edu>
2025-12-19 19:21:23 -0800

    updated .gitignore

--- engine/main.cpp
+++ engine/main.cpp
@@ -3,13 +3,9 @@
 
 int main() {
 
-    BoardState board;
-
-    board.bBishop = 292092ULL;
-    board.bKing = 2002ULL;
-
-    std::cout << "hello" << std::endl;
- 
+    std::string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    
+    GameState *game = new GameState(fen);
 
     return 0;
 }
