bec619de83c0d22395ca4c940c0dcd34a475bf96
gmcgehee <geist.mcgehee@eagles.cui.edu>
2025-12-23 23:43:51 -0800

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include "bitboard.h"
 
-int main() {
+int main()
+{
 
-    std::string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 12 122";
-    
-    GameState *game = new GameState(fen);
+    //std::string fenIn = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq e3 12 122";
+    std::string fenIn = "r3k2r/ppp1n1pp/2n2p1q/PPN1B1qb/2p2rpP/8/8/R3K3 b Qkq h3 0 1";
+                      // r3k2r/ppp1n1pp/2n2p1q/PPN1B1qb/2p2rpP/8/8/R3K3 b Qkq h3 0 1
 
-    // game->printBoardState(BoardState game.state); // idk how to make this work
+    GameState *game = new GameState(fenIn);
 
-    // int test_square = game->squareToNumber("g2");
-    // std::cout << "Test square: " << test_square << std::endl;
+    std::string fenOut = game->exportFen(game->state);
+
+    std::cout << "FEN Out: " << fenOut << '\n';
 
     std::cout << "Program ran successfully!" << '\n';
-
     return 0;
 }
